ac42ba9336d08801ba35814f24bb8df6b10e937e
Gubina Irina <irina.gubina98@mail.ru>
2020-09-08 16:27:28 +0300

    Добавлен src/

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int TicketPrice = 14879;
+	int BonusMile = TicketPrice / 20;
+	System.out.println(BonusMile);
+    }
+}
